ab1f3053a6bfec2d532a66e5c151f49ef64e388e
smile-core <smileshalom74@gmail.com>
2025-05-18 10:42:52 +0530

    fixed divide zero error

--- src/util/math.js
+++ src/util/math.js
@@ -23,5 +23,8 @@ export function divide(x, y) {
   if (typeof x !== "number" || typeof y !== "number") {
     throw new TypeError("Both parameters must be numbers");
   }
+  if (y === 0) {
+    throw new Error("Cannot divide by zero");
+  }
   return x / y;
 }
