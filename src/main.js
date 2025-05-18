db414b8883886cd33544c3a9ab85a9cfd6011868
ziya-celebi <ziyacelebi30@gmail.com>
2025-05-19 04:45:59 +0530

    user sees grid of btns now

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
-import "./ui/createButtons";
-import { createButtons } from "./ui/createButtons";
 import "./style.css";
+import { createButtons } from "./ui/createButtons";
+import { CreateContainer } from "./ui/CreateContainer";
 
-createButtons();
+let obj1 = new createButtons();
+let obj2 = new CreateContainer();
+
+document.body.appendChild(obj2.container);
+for (let x = 0; x < obj1.buttonsContext.length; x++) {
+  obj2.container.appendChild(obj1.buttons[x]);
+}
