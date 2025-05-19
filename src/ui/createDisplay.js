6c5b21011e9e0c702627dedb0325c4100ca036f2
smile-core <smileshalom74@gmail.com>
2025-05-19 12:32:08 +0530

    created display

---
+++ src/ui/createDisplay.js
@@ -0,0 +1,6 @@
+export class CreateDisplay {
+  constructor() {
+    this.display = document.createElement("input");
+    this.display.className = "display";
+  }
+}
--- src/ui/initUi.js
+++ src/ui/initUi.js
@@ -1,13 +1,22 @@
 import { createButtons } from "./createButtons";
 import { CreateContainer } from "./CreateContainer";
+import { CreateDisplay } from "./createDisplay";
 
 let obj1 = new createButtons();
 let obj2 = new CreateContainer();
+let obj3 = new CreateDisplay();
 
 //TO ADD CONTAINER TO HTML AND ADD BUTTONS TO CONTAINER
 export function initUI() {
   document.body.appendChild(obj2.container);
+  document.body.appendChild(obj3.display);
   for (let x = 0; x < obj1.buttonsContext.length; x++) {
     obj2.container.appendChild(obj1.buttons[x]);
   }
+
+  obj2.container.addEventListener("click", function (event) {
+    if (event.target.tagName === "BUTTON") {
+      obj3.display.value = event.target.textContent;
+    }
+  });
 }
