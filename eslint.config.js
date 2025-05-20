45f75b9a457c2274068c01f0ce77900d22b035b3
ziya-celebi <ziyacelebi30@gmail.com>
2025-05-21 04:09:10 +0530

    package resolved

--- eslint.config.js
+++ eslint.config.js
@@ -17,6 +17,7 @@ export default defineConfig([
   },
   {
     files: ["**/*.json"],
+    ignores: ["**/package-lock.json"],
     plugins: { json },
     language: "json/json",
     extends: ["json/recommended"],
