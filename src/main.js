df8bdc70f48895d549d9ebd7467f82cfba1f44db
ziya-celebi <ziyacelebi30@gmail.com>
2025-05-20 11:58:39 +0530

    button handler added

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { initUI } from "./ui/initUi";
 import "./style.css";
+import "./ui/UIElements";
 
 initUI();
--- src/ui/initUi.js
+++ src/ui/initUi.js
@@ -9,13 +9,34 @@ const buttons = createButtons();
 const bContainer = createBContainer();
 const appContainer = createAppContainer();
 const display = createDisplay();
+let currentValue = "";
 
 //TO ADD APPCONT TO HTML, ADD DISPLAY AND BUTTONCONT TO APPCONT, ADD BUTTONS TO BUTTONCONT
-export function initUI() {
+function initGUI() {
   document.body.appendChild(appContainer);
   appContainer.appendChild(display);
   appContainer.appendChild(bContainer);
   for (let x = 0; x < buttons.length; x++) {
     bContainer.appendChild(buttons[x]);
   }
+}
+
+//LISTENS CLICK EVENTS ON BUTTONS, ON CLICK CURRENTVALUE BECOMES BUTTON CONTENT
+function InputHandler() {
+  for (let x = 0; x < buttons.length; x++) {
+    buttons[x].addEventListener("click", function (event) {
+      currentValue = event.target.textContent;
+    });
+  }
+}
+
+//RETURNS THE CURRENT BUTTONS VALUE
+export function getValue() {
+  return currentValue;
+}
+
+//INITALIZES GUI AND INPUT HANDLER
+export function initUI() {
+  initGUI();
+  InputHandler();
 }
